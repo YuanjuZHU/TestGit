82acf59cd2b988861ea844ccbb4bce4eaa1d64fa
YuanjuZHU <eliteyjz@gmail.com>
2022-03-16 10:41:51 +0100

    A test for "Scriptable object".

---
+++ Assets/Yuanju/ActuatorData/ActuatorDataSO.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace Assets.Yuanju.ActuatorData
+{
+    [CreateAssetMenu(menuName = "Data/ActuatorData")]
+    public class ActuatorDataSO : ScriptableObject
+    {
+        public bool HasRigidBody;
+        public bool HasInteractionBehaviour;
+        public bool HasHingeJoint;
+    }
+}
+
