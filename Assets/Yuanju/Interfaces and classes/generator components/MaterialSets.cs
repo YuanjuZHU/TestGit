1ce71bc76b4d705152820c443cba9270cb3a3ad7
YuanjuZHU <eliteyjz@gmail.com>
2022-06-01 10:23:50 +0200

    Create a class the manage elements that have different parts, each part has a unique color.

---
+++ Assets/Yuanju/Interfaces and classes/generator components/MaterialSets.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MaterialSets : MonoBehaviour
+{
+    /// <summary>
+    /// a switch have several status, for each status, there should be a set of materials 
+    /// </summary>
+    [System.Serializable]
+    public class StatusMaterialSets
+    {
+        //[SerializeField]
+        public List<PartsMaterialSets> MaterialSet = new List<PartsMaterialSets>(); /*{ get; set; }*/
+
+    }
+
+    /// <summary>
+    /// a switch may have several parts
+    /// </summary>
+    [System.Serializable]
+    public class PartsMaterialSets
+    {
+        //[SerializeField]
+        [HideInInspector]
+        public string FontName;
+
+        public List<Material> MaterialsOfDifferentParts = new List<Material>(); 
+    }
+}
