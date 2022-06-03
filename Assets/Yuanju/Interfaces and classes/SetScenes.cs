c844c8b56f905f1947a8d27397fdaa59ebaf9f9f
YuanjuZHU <eliteyjz@gmail.com>
2022-06-03 19:56:42 +0200

    SetScene.cs without datatables.

--- Assets/Yuanju/Interfaces and classes/SetScenes.cs
+++ Assets/Yuanju/Interfaces and classes/SetScenes.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Assets.Yuanju.Interfaces_and_classes.generator_components;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,7 +8,7 @@ using UnityEngine;
 public class SetScenes : MonoBehaviour
 {
     public static Dictionary<string, IGeneratorComponent> ScriptComponents= new Dictionary<string, IGeneratorComponent>();
-    public static List<String> GeneratorElements=new List<String>(); //attention here the operable components is the same as the list OperableComponentNames, but the names are in different order
+    //public static List<String> GeneratorElements=new List<String>(); //attention here the operable components is the same as the list OperableComponentNames, but the names are in different order
     public static Dictionary<string, GameObject> NonSettableElements = new Dictionary<string, GameObject>(); //the elements like the water tank cover, which don't have object class attached
     void Awake()
     {
@@ -30,18 +31,21 @@ public class SetScenes : MonoBehaviour
         }
     }
 
+    /// <summary>
+    /// in this method, the datatables has been replaced
+    /// </summary>
     void Update()
     {
         if (!PanelManager.isGeneratorConfigured || (Input.GetKeyDown(KeyCode.A)))
         {
-            for (int i = 0; i < NPOIReadExcel.SubTasks.Count; i++)
+            for (int i = 0; i < SteamGenerator.SubTasks.Count()/*NPOIGetInitialFinalSettings.SubTasks.Count*/; i++)
             {
                 //TODO needs to implement for the italian version
-                if (NPOIReadExcel.SubTasks[i].Contains(PanelManager.listToggleText[2]))
+                if (SteamGenerator.SubTasks.ElementAt(i)/*NPOIGetInitialFinalSettings.SubTasks[i]*/.Contains(PanelManager.listToggleText[2]))
                 {
-                    Debug.Log("the task in the excel: " + NPOIReadExcel.SubTasks[i]);
+                    Debug.Log("the task in the excel: " + SteamGenerator.SubTasks.ElementAt(i));
                     Debug.Log("the task in the toggle: " + PanelManager.listToggleText[2]);
-                    PopSettings(NPOIReadExcel.SubTasks[i]);
+                    PopSettings(SteamGenerator.SubTasks.ElementAt(i));
                 }
             }
             PanelManager.isGeneratorConfigured = true;
@@ -60,8 +64,10 @@ public class SetScenes : MonoBehaviour
             {
                 //Debug.Log("the name off the4 gameobject 1: "+ GameObject.Find(Name).ToString());
                 ScriptComponents.Add(Name, GameObject.Find(Name).GetComponentInChildren<IGeneratorComponent>());
-                GeneratorElements.Add(Name);
-                Debug.Log("the generator elements: "+ Name);
+                //GeneratorElements.Add(Name);
+                Debug.Log("GameObject.Find(Name).GetComponentInChildren<IGeneratorComponent>(): " + Name);
+                Debug.Log("GameObject.Find(Name).GetComponentInChildren<IGeneratorComponent>(): " + GameObject.Find(Name).GetComponentInChildren<IGeneratorComponent>());
+
             }
             else
             {
@@ -77,17 +83,18 @@ public class SetScenes : MonoBehaviour
     /// </summary>
     private void PopSettings(string task)
     {
-        foreach (var Name in GeneratorElements)
+        var settings = SteamGenerator.InitialSettings.Where(x => x.SubTask == task).ElementAt(0);
+        foreach (var Name in SteamGenerator.InitialSettingElements/*GeneratorElements*/)
         {
             //TODO if the name exist in the dictionary's key words
-            Debug.Log("task: " + task);
-            Debug.Log("Name: " + Name);
-            //Debug.Log("NPOIReadExcel.TaskComponentInitialSettings[task][Name]: "+ NPOIReadExcel.TaskComponentInitialSettings[task][Name]);
-            Debug.Log("ScriptComponents[Name]: " + ScriptComponents[Name]);
-            ScriptComponents[Name].Status = NPOIReadExcel.TaskComponentInitialSettings[task][Name];
+            //Debug.Log("NPOIGetInitialFinalSettings.TaskComponentInitialSettings[task][Name]: "+ NPOIGetInitialFinalSettings.TaskComponentInitialSettings[task][Name]);
+            if (settings.ElementStatus[Name].Status!=null)
+            {
+                ScriptComponents[Name].Status = (int)settings.ElementStatus[Name].Status; /*NPOIGetInitialFinalSettings.TaskComponentInitialSettings[task][Name];*/
+            }
             ScriptComponents[Name].UpdateMaterials(); //indispensable, sometimes the previous status is the same as the status to pop in
             //Debug.Log("the actuators names: " + Name);
-            //Debug.Log("the actuators supposed status: " + NPOIReadExcel.TaskComponentInitialSettings[task][Name]);
+            //Debug.Log("the actuators supposed status: " + NPOIGetInitialFinalSettings.TaskComponentInitialSettings[task][Name]);
 
             //TODO should update the "status" of the non operable components, like change the material for the LEDs. 
             //Debug.Log(" NPOIReadExcelOld.TaskComponentInitialSettings[NPOIReadExcelOld.SubTasks[SceneNumber]][Name]: " + NPOIReadExcelOld.TaskComponentInitialSettings[NPOIReadExcelOld.SubTasks[SceneNumber]][Name]);
@@ -95,7 +102,7 @@ public class SetScenes : MonoBehaviour
 
         //foreach (var VARIABLE in ReadCSV.componentNames)
         //{
-        //    if (!NPOIReadExcel.TaskCompoentInitialSettings[NPOIReadExcel.SubTasks[0]].Keys.Contains(VARIABLE))
+        //    if (!NPOIGetInitialFinalSettings.TaskCompoentInitialSettings[NPOIGetInitialFinalSettings.SubTasks[0]].Keys.Contains(VARIABLE))
         //        Debug.Log("!ReadCSV.componentNames.Contains(VARIABLE): " + VARIABLE);
 
         //}
